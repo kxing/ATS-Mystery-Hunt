1b653bddf6fd835a8ad90dc45117f1e4dd611a22
Kerry Xing <kxing@mit.edu>
2012-12-26 18:36:27 -0800

    Implemented the validator function for the simple Nurikabe puzzle.

--- tests/nurikabe_simple.cpp
+++ tests/nurikabe_simple.cpp
@@ -24,23 +24,23 @@
 //
 // We encode the squares as shown below:
 //
-//  0  1  2  3  4  5
-//  6  7  8  9 10 11
-// 12 13 14 15 16 17
-// 18 19 20 21 22 23
-// 24 25 26 27 28 29
-// 30 31 32 33 34 35
+//  0  1  2  3  4
+//  5  6  7  8  9
+// 10 11 12 13 14
+// 15 16 17 18 19
+// 20 21 22 23 24
 // -----------------------------------------------------------------------------
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "include/board.h"
 #include "include/state.h"
 #include "include/state_list.h"
 
-const int NUMBER_OF_ROWS = 6;
-const int NUMBER_OF_COLUMNS = 6;
+const int NUMBER_OF_ROWS = 5;
+const int NUMBER_OF_COLUMNS = 5;
 const int NUMBER_OF_SQUARES = NUMBER_OF_ROWS * NUMBER_OF_COLUMNS;
 const int NUMBER_OF_STATES = 2;
 
@@ -57,15 +57,153 @@ const State* STATES[NUMBER_OF_STATES] = {
 
 const StateList STATE_LIST(STATES, NUMBER_OF_STATES);
 
+const int NUMBER_OF_DIRECTIONS = 4;
+const int delta_x[NUMBER_OF_DIRECTIONS] = {1, 0, -1, 0};
+const int delta_y[NUMBER_OF_DIRECTIONS] = {0, 1, 0, -1};
+
+int row_of_square(int square) {
+  return square / NUMBER_OF_COLUMNS;
+}
+
+int column_of_square(int square) {
+  return square % NUMBER_OF_COLUMNS;
+}
+
+int row_and_column_to_square(int row, int column) {
+  return row * NUMBER_OF_COLUMNS + column;
+}
+
+const int VARIABLE = 9999;
+bool can_match_group_size(const Board* const board,
+                          const State* const state_type,
+                          int square,
+                          int target,
+                          bool* checked) {
+  if (board->get_value(square) != state_type &&
+      board->get_value(square) != EMPTY) {
+    return false;
+  }
+
+  int queue[NUMBER_OF_SQUARES];
+  int length_of_queue = 0;
+  int processed = 0;
+  int counter = 0;
+
+  queue[length_of_queue++] = square;
+  checked[square] = true;
+  counter++;
+
+  bool tentative_accept = false;
+  while (processed < length_of_queue) {
+    for (int i = 0; i < NUMBER_OF_DIRECTIONS; i++) {
+      int next_row = row_of_square(queue[processed]) + delta_x[i];
+      int next_column = column_of_square(queue[processed]) + delta_y[i];
+
+      if (0 <= next_row && next_row < NUMBER_OF_ROWS &&
+          0 <= next_column && next_column < NUMBER_OF_COLUMNS) {
+        int next_square = row_and_column_to_square(next_row, next_column);
+
+        if (board->get_value(next_square) == state_type) {
+          if (checked[next_square]) {
+            // We've already processed this.
+            continue;
+          }
+          queue[length_of_queue++] = next_square;
+          checked[next_square] = true;
+          counter++;
+          if (counter > target) {
+            return false;
+          }
+        } else if (board->get_value(next_square) == EMPTY) {
+          tentative_accept = true;
+        }
+      }
+    }
+    processed++;
+  }
+  if (tentative_accept) {
+    return true;
+  }
+
+  return (counter == target);
+}
+
 bool validator(const Board* const board) {
-  // TODO(kxing): Add in the constraints.
+  bool checked[NUMBER_OF_SQUARES];
+  memset(checked, 0, sizeof(checked));
+
+  // The number 2 in the first row, second column.
+  if (!can_match_group_size(board,
+                            FILLED,
+                            row_and_column_to_square(0, 1),
+                            2,
+                            checked)) {
+    return false;
+  }
+  // The number 2 in the first row, fifth column.
+  if (!can_match_group_size(board,
+                            FILLED,
+                            row_and_column_to_square(0, 4),
+                            2,
+                            checked)) {
+    return false;
+  }
+  // The number 3 in the third row, fourth column.
+  if (!can_match_group_size(board,
+                            FILLED,
+                            row_and_column_to_square(2, 3),
+                            3,
+                            checked)) {
+    return false;
+  }
+  // The number 2 in the fourth row, third column.
+  if (!can_match_group_size(board,
+                            FILLED,
+                            row_and_column_to_square(3, 2),
+                            2,
+                            checked)) {
+    return false;
+  }
+
+  // Check that there aren't any uncounted islands.
+  for (int i = 0; i < NUMBER_OF_SQUARES; i++) {
+    if (board->get_value(i) == FILLED && !checked[i] &&
+        !can_match_group_size(board, FILLED, i, VARIABLE, checked)) {
+      return false;
+    }
+  }
+
+  // The no-lakes rule.
+  for (int i = 1; i < NUMBER_OF_ROWS; i++) {
+    for (int j = 1; j < NUMBER_OF_COLUMNS; j++) {
+      if (board->get_value(row_and_column_to_square(i, j)) == UNFILLED &&
+          board->get_value(row_and_column_to_square(i - 1, j)) == UNFILLED &&
+          board->get_value(row_and_column_to_square(i, j - 1)) == UNFILLED &&
+          board->get_value(row_and_column_to_square(i - 1, j - 1)) ==
+              UNFILLED) {
+        return false;
+      }
+    }
+  }
+
+  // The contiguous stream rule.
+  for (int i = 0; i < NUMBER_OF_SQUARES; i++) {
+    if (board->get_value(i) == UNFILLED && !checked[i] &&
+        !can_match_group_size(board,
+                              UNFILLED,
+                              i,
+                              NUMBER_OF_SQUARES - 9,
+                              checked)) {
+      return false;
+    }
+  }
   return true;
 }
 
 int main() {
   int search_order[NUMBER_OF_SQUARES] = {
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+    21, 22, 23, 24,
   };
 
   Board board(NUMBER_OF_SQUARES, &STATE_LIST, search_order, &validator);
